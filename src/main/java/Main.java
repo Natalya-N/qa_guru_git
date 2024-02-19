71db2cdbf99fc90b29be91f95d09fe9775c9e546
Natalya-N <58128230+Natalya-N@users.noreply.github.com>
2024-02-19 21:55:14 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,8 +2,10 @@ import java.sql.SQLOutput;
 
 public class Main {
 
+    String message = "Hello world!"
+
     public static void main(String[] args) {
-        System.out.println("Hi");
+        System.out.println(message);
     }
 
 

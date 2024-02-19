52b0dba8b4ec712d6de5d31b2d8556d8c1d2c19d
Natalya-N <58128230+Natalya-N@users.noreply.github.com>
2024-02-19 22:00:47 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,8 @@ import java.sql.SQLOutput;
 
 public class Main {
 
+    String testMessage = "Test message"
+    
     String message = "Hello world!"
 
     public static void main(String[] args) {
